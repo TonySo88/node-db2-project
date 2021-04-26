8ad4b71269d1ab8df36e45db5c00f7903016d5a4
Tony So <60905365+TonySo88@users.noreply.github.com>
2021-04-26 18:12:46 -0500

    mvp

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,68 @@
-const checkCarId = (req, res, next) => {
+const model = require("./cars-model")
+const dbConfig = require('../../data/db-config')
+var vinValidator = require('vin-validator')
+
+const checkCarId = () => async (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    const idCheck = await model.getById(req.params.id)
+      if(idCheck) {
+        next()
+      } else {
+        res.status(404).json({
+          message: `Car with ID ${req.params.id} could not be found`
+        })
+      }
+  } catch(err) {
+    next(err)
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
+const checkCarPayload = () => async (req, res, next) => {
   // DO YOUR MAGIC
+  if (!req.body.mileage || !req.body.vin || !req.body.make || !req.body.model ) {
+    return status(400).json({
+      message: "Required field is missing"
+    })
+  }
+
+  next()
 }
 
-const checkVinNumberValid = (req, res, next) => {
+const checkVinNumberValid = () => (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+    var validVin = vinValidator.validate(req.body.vin)
+
+    if(validVin === true) {
+      next()
+    } else {
+      res.status(400).json({
+        message: `${validVin} is invalid`
+      })
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
+const checkVinNumberUnique = () => async (req, res, next) => {
   // DO YOUR MAGIC
+  const allCars = await model.getAll()
+  const vinCheck = allCars.filter(car => car.vin === req.body.vin)
+
+  if(vinCheck[0] === req.body.vin) {
+    return res.status(400).json({
+      message: "VIN already exists"
+    })
+  } else {
+    next()
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,43 @@
-const getAll = () => {
+const db = require("../../data/db-config")
+
+const getAll = async () => {
   // DO YOUR MAGIC
+  const cars = await db
+    .select("*")
+    .from("cars")
+
+    return cars
 }
 
-const getById = () => {
+const getById = async (id) => {
   // DO YOUR MAGIC
+  const car = await db
+    .select("*")
+    .from("cars")
+    .where("id", id)
+    .first()
+
+    return car
 }
 
-const create = () => {
+const create = async (id, car) => {
   // DO YOUR MAGIC
+  const post = await db
+    .insert({
+      vin: car.vin,
+      make: car.make,
+      model: car.model,
+      mileage: car.mileage,
+      title: car.title,
+      transmission: car.transmission
+    })
+    .into("cars")
+
+    return post
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
 }
